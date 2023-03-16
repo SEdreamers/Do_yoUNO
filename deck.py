99c118c7552e31788bf751b7607e2e1765eb0816
Limchaeyoung <chaeinge@gmail.com>
2023-03-17 02:43:20 +0900

    refactor: 기본 모드/색약 모드 표현 수정

--- deck.py
+++ deck.py
@@ -2,21 +2,21 @@ import random
 from card import Card
 
 class Deck:
-    def __init__(self, color_blind_mode):
+    def __init__(self):
         self.cards = []
-        self.load_cards(color_blind_mode)
+        self.load_cards()
     
-    def load_cards(self, color_blind_mode):
+    def load_cards(self):
         values = [str(i) for i in range(0, 10)] + ["skip", "reverse", "draw2", "draw4"]
         colorless_values = ["wild", "wild_draw4", "wild_swap"]
         colors = ["red", "green", "blue", "yellow"]
         for color in colors:
             for value in values:
-                card = Card(value, color, color_blind_mode)
+                card = Card(value, color)
                 self.cards.append(card)
         for value in colorless_values:
             for _ in range(4):
-                card = Card(value, "black", color_blind_mode)
+                card = Card(value, "black")
                 self.cards.append(card)
                 
     def shuffle(self):
